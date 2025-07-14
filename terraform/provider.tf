0336d20d29a18e7d08ca2643ebf19b604e1f148f
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2025-07-15 00:15:11 +0300

    feat: add providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,28 @@
 terraform {
   required_providers {
     yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.84.0"
+      source = "yandex-cloud/yandex"
+    }
+    datadog = {
+      source = "DataDog/datadog"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
     }
   }
+  required_version = ">= 0.13"
 }
 
 provider "yandex" {
+  zone      = var.yc_zone
   token = var.yc_iam_token
   cloud_id  = var.yc_cloud_id
   folder_id = var.yc_folder_id
-  zone      = var.yc_zone
+}
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = "https://api.datadoghq.eu/"
 }
